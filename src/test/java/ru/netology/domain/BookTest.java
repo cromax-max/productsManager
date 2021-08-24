cf2bfec4d918618285c4f05f1ae2c4091b96d34b
Sergey <tel.cromax@gmail.com>
2021-08-25 10:58:20 +1100

    ?new files Book/SmartphoneTest

---
+++ src/test/java/ru/netology/domain/BookTest.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookTest {
+
+    @Test
+    void matches() {
+    }
+}
---
+++ src/test/java/ru/netology/domain/SmartphoneTest.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SmartphoneTest {
+
+    @Test
+    void matches() {
+    }
+}
