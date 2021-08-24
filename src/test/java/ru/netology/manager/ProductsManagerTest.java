2ed047fe912de152edcf2d21f2792cf77f6bc680
Sergey <tel.cromax@gmail.com>
2021-08-24 16:30:10 +1100

    Update tests

--- src/test/java/ru/netology/manager/ProductsManagerTest.java
+++ src/test/java/ru/netology/manager/ProductsManagerTest.java
@@ -21,34 +21,48 @@ class ProductsManagerTest {
     ProductsRepository repository;
     @InjectMocks
     ProductsManager manager;
-    Product book = new Book(101, "Java", 800, "Yakov Fain");
+    Product book1 = new Book(101, "Java", 800, "Horstmann");
+    Product book2 = new Book(102, "Java", 800, "Joshua Bloch");
     Product phone1 = new Smartphone(121, "P30", 1300, "Orange");
     Product phone2 = new Smartphone(122, "P50", 1000, "Orange");
 
     @BeforeEach
     void setup() {
-        manager.add(book);
+        manager.add(book1);
+        manager.add(book2);
         manager.add(phone1);
         manager.add(phone2);
 
-        Product[] returned = {book, phone1, phone2};
+        Product[] returned = {book1, book2, phone1, phone2};
         doReturn(returned).when(repository).findAll();
     }
 
     @Test
     void shouldSearchEmpty() {
         Product[] expected = new Product[0];
-        assertArrayEquals(expected, manager.searchBy("Phyton"));
+        assertArrayEquals(expected, manager.searchBy("Space"));
     }
 
     @Test
-    void shouldSearchOne() {
-        Product[] expected = {book};
+    void shouldSearchByBookName() {
+        Product[] expected = {book1, book2};
         assertArrayEquals(expected, manager.searchBy("Java"));
     }
 
     @Test
-    void shouldSearchSeveral() {
+    void shouldSearchByBookAuthor() {
+        Product[] expected = {book2};
+        assertArrayEquals(expected, manager.searchBy("Joshua Bloch"));
+    }
+
+    @Test
+    void shouldSearchBySmartphoneName() {
+        Product[] expected = {phone1};
+        assertArrayEquals(expected, manager.searchBy("P30"));
+    }
+
+    @Test
+    void shouldSearchBySmartphoneManufacturer() {
         Product[] expected = {phone1, phone2};
         assertArrayEquals(expected, manager.searchBy("Orange"));
     }
