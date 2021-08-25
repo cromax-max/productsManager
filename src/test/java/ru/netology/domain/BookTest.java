3e0afda41835eac3aff64d0f481429fa51fa46c5
Sergey <tel.cromax@gmail.com>
2021-08-25 21:13:35 +1100

    update Book/SmartphoneTest

--- src/test/java/ru/netology/domain/BookTest.java
+++ src/test/java/ru/netology/domain/BookTest.java
@@ -1,12 +1,22 @@
 package ru.netology.domain;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookTest {
+    Product book = new Book(101, "Java", 800, "Horstmann");
 
-    @Test
-    void matches() {
+    @ParameterizedTest
+    @CsvSource({"Java", "Horstmann"})
+    void shouldBeExistent (String text) {
+        assertTrue(book.matches(text));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"Ruby", "Yakov Fain"})
+    void shouldBeNonExistent(String text) {
+        assertFalse(book.matches(text));
     }
 }
--- src/test/java/ru/netology/domain/SmartphoneTest.java
+++ src/test/java/ru/netology/domain/SmartphoneTest.java
@@ -1,12 +1,23 @@
 package ru.netology.domain;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SmartphoneTest {
 
-    @Test
-    void matches() {
+    Product phone = new Smartphone(121, "P30", 1300, "Orange");
+
+    @ParameterizedTest
+    @CsvSource({"P30", "Orange"})
+    void shouldBeExistent(String text) {
+        assertTrue(phone.matches(text));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"I45", "Samsung"})
+    void shouldBeNonExistent(String text) {
+        assertFalse(phone.matches(text));
     }
 }
